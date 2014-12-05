e3ae493819b3c4ca5534c421d7a16e3378af2990
valmaev <almaev.vladimir@gmail.com>
2014-12-05 17:51:57 +0300

    Initial solution structure

---
+++ DotNext.RockAndRoll/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNext.RockAndRoll")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: Guid("40d521a2-efe0-4065-aa9e-10fe2db11368")]

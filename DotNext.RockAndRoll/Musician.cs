eddd34425fd401fdeeb7205ac25d47bfbc149a88
valmaev <almaev.vladimir@gmail.com>
2014-12-05 17:53:10 +0300

    Added Musician class

---
+++ DotNext.RockAndRoll/Musician.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNext.RockAndRoll
+{
+    public class Musician
+    {
+        private readonly string _firstName;
+        private readonly string _lastName;
+
+        public Musician(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+        }
+    }
+}
